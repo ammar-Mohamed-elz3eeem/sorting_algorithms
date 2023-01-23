71f16ff1b70e8469066b70373a2aa4a447150e0a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-23 22:42:37 +0200

    add project file + sorting algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function thay sorts array using bubble sort Algorithm
+ * @array: the array to be sorted
+ * @size: number of elements in that array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int i;
+	int sorted = SORTED;
+
+	while (!sorted)
+	{
+		sorted = NOT_SORTED;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i + 1] < array[i])
+			{
+				int tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				sorted = SORTED;
+			}
+		}
+		print_array(array, size);
+	}
+}
